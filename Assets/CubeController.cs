6712dec83d2fcbd7899c1d6828ece2a434f6ae17
Yuki Nishimura <yukinkopen@gmail.com>
2022-03-24 22:25:04 +0900

    Lesson7 課題に対応

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -8,9 +8,12 @@ public class CubeController : MonoBehaviour
 
     private float deadLine = -10;
 
+    public AudioClip blockSound;
+    AudioSource audioSource;
+
     void Start()
     {
-
+        audioSource = GetComponent<AudioSource>();
     }
 
     void Update()
@@ -20,6 +23,15 @@ public class CubeController : MonoBehaviour
         if (transform.position.x < this.deadLine)
         {
             Destroy(gameObject);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        string collidedName = collision.gameObject.name;
+        if (collidedName.StartsWith("CubePrefab") || collidedName == "ground")
+        {
+            audioSource.PlayOneShot(blockSound);
         }
     }
 }
